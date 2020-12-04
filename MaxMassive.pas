d35c1bf44cd6016e40acb45391e62cc25f755b03
Klim Romanov <klr0manov@yandex.ru>
2020-12-04 13:52:10 +0500

    Programm

---
+++ MaxMassive.pas
@@ -0,0 +1,26 @@
+﻿const
+  n = 10;
+ 
+var
+  i, k, max: integer;
+  y: array[1..n] of integer;
+ 
+begin
+  randomize;
+  writeln('Массив, положительные значения ');
+  max := 0;
+  for i := 1 to n do
+  begin
+    y[i] := -n div 2 + random(n);
+    write(y[i]:4);
+    if (y[i] > 0) then
+      write(y[i]:4);
+    if (y[i] > 0) and (y[i] > max) then
+      max := y[i];
+    writeln;
+  end;
+  if max <> 0 then
+    writeln('Max: ', max)
+  else
+    writeln('Нет положительных чисел.');    
+end.
---
+++ Mod5Number.pas
@@ -0,0 +1,23 @@
+﻿const
+  n = 10;
+ 
+var
+  i, k, count: integer;
+  y: array[1..n] of integer;
+ 
+begin
+  randomize;
+  writeln('Массив, числа кратные 5');
+  for i := 1 to n do
+  begin
+    y[i] := random(100);
+    write(y[i]:4);
+    if y[i] mod 5 = 0 then
+      begin
+      write(y[i]:10);
+      inc(count);
+      end;
+    writeln;
+  end;
+  write('Количество чисел кратных 5: ', count);
+end.
